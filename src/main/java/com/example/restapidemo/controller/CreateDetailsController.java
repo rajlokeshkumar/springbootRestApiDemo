8ebf319bbc574308eb150135d912e2fa1eea9845
Kamalakannan <kamalkuppus@gmail.com>
2022-10-02 08:39:05 +0100

    Newly Added files, push changes to origin

---
+++ src/main/java/com/example/restapidemo/controller/CreateDetailsController.java
@@ -0,0 +1,33 @@
+package com.example.restapidemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restapidemo.dto.EmployeeRequestDto;
+import com.example.restapidemo.service.DetailsService;
+
+@RestController
+public class CreateDetailsController {
+	
+	@Autowired
+	private DetailsService detailsService;
+	
+//    @GetMapping("/newEmployee")
+//    public String add(Model model) {
+//        model.addAttribute("employee", new EmployeeRequestDto());
+//        return "new";
+//    }
+	
+	@PostMapping(path = "createEmployee", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> processUpdate(@RequestBody EmployeeRequestDto employeeRequestDto) {
+		System.out.println("employeeRequestDto: "+employeeRequestDto);
+		detailsService.saveEmployee(employeeRequestDto);
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/example/restapidemo/controller/DeleteDetailsController.java
@@ -0,0 +1,28 @@
+package com.example.restapidemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restapidemo.service.DetailsService;
+
+@RestController
+public class DeleteDetailsController {
+
+	@Autowired
+	private DetailsService detailsService;
+	
+	@DeleteMapping(path = "deleteEmployee/{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable(value = "id") String id){
+		
+		String msg = detailsService.deleteEmployee(id);
+		
+		System.out.println("DELETE MESSAGE: "+msg);
+		
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
+	
+}
