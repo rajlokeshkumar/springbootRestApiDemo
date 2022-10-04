49aae9ed05cf8c37a3ee943462225e7c4c907c97
Kamalakannan <kamalkuppus@gmail.com>
2022-10-04 20:08:53 +0100

    Created new package com.example.kamak and added new file called TestFile.java

---
+++ src/main/java/com/example/kamal/TestFile.java
@@ -0,0 +1,12 @@
+/*
+ * @author: Kamalakannan Kuppusamy 
+ * @date: 04/10/2022
+ * @Note: Test file created to check git commit.
+ * 
+ */
+
+package com.example.kamal;
+
+public class TestFile {
+	
+}
