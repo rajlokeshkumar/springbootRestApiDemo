ae457902a339bf85575e85e4cfa8399f58982a4e
DESKTOP-1O5J0SB\loki <rajlokeshkumar@gmail.com>
2022-10-05 17:05:49 +0530

    Exception handling

---
+++ src/main/java/com/example/restapidemo/exception/RollNumberNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.restapidemo.exception;
+
+public class RollNumberNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	
+	
+}
